f087b67db70b3306f7e5d61e9e6064363d10bb1c
ImDeepakBari <mfsi.deepak.bari@gmail.com>
2024-09-15 16:01:46 +0530

    added comments for tc

--- sync_api.py
+++ sync_api.py
@@ -1,10 +1,11 @@
 from playwright.sync_api import sync_playwright
 
 with sync_playwright() as sync_p:
+    # assigning browser
     browser = sync_p.firefox.launch(headless=False, slow_mo=500)
 
-    page = browser.new_page()
-    page.goto("https://www.hackerrank.com/")
+    page = browser.new_page()  # creating a page for browser context
+    page.goto("https://www.hackerrank.com/")  # navigating to url
     assert 'HackerRank - Online Coding Tests and Technical Interviews' == page.title()
     browser.close()
 
